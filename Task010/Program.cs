c8b9094dd4d3be37caa583f699a57c0392c00ad0
Vitaliy <vinogradovvitaliy2013@gmail.com>
2022-08-12 23:58:26 +0300

    Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Пожалуйста введите положительное трёхзначное целое число и нажмите Enter:");
+string? numberString = Console.ReadLine()!;
+int threeDigitNumber = int.Parse(numberString);
+
+int twoDigitNumber = threeDigitNumber / 10;
+int ourNumber = twoDigitNumber % 10;
+
+if (threeDigitNumber > 99 && threeDigitNumber <= 999)
+{
+    Console.WriteLine("Вторая цифра этого числа " + ourNumber);
+}
+else if (threeDigitNumber < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+else
+{
+    Console.WriteLine("Вы ввели НЕ трехзначное число");
+}
